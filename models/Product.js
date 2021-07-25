4cbb2c6c84ad5263bf0c1451872cbf80ac1198a5
Kayla Dombrosky <kdombroskyy@live.com>
2021-07-25 12:14:13 -0400

    setting up models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      //type: //decimal, 
+      allowNull: false
+      //validate value is decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+      // set default value to 10
+      // validate that value is numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER
+      // ref category model id
+    }
   },
   {
     sequelize,
